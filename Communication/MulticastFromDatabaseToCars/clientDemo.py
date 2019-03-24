d8df2ed75e90516691306a8aa9c24b6790dc0225
PengChen <fengyi_pc@foxmail.com>
2019-03-25 00:29:29 +0800

    Update clientDemo.py

--- Communication/MulticastFromDatabaseToCars/clientDemo.py
+++ Communication/MulticastFromDatabaseToCars/clientDemo.py
@@ -83,13 +83,10 @@ if __name__=='__main__':
     c.bind((Client_Addr, CLIENT_PORT))
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    rs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-    rs.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_LOOP, 1) 
 
     s.bind((ANY,MULTICAST_PORT))
-    rs.bind((SERVER_ADDR, MULTICAST_PORT))
     s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 10)
     s.setsockopt(socket.IPPROTO_IP,
                  socket.IP_ADD_MEMBERSHIP,

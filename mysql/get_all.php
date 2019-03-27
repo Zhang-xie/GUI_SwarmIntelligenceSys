337ce6b51e1d80f5b8663ce1042eda209e0c266d
Zhang-xie <46151826+Zhang-xie@users.noreply.github.com>
2019-03-27 18:34:57 +0800

    Update get_all.php

--- mysql/get_all.php
+++ mysql/get_all.php
@@ -1,11 +1,21 @@
-<!-- 获取每个ID下的最新数据 -->
-
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "swarmintelligence";
  
+ class User
+ {
+    public $ID;
+    public $X;
+    public $Y;
+    public $Z;
+    public $speed;
+    public $pitch;
+    public $roll;
+    public $azimuth;
+    public $time;
+ }
 // 创建连接
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -25,15 +35,28 @@ $sql = "SELECT en.ID, en.X, en.Y,en.Z,en.speed,en.pitch,en.roll,en.azimuth,en.ti
 
 $result = $conn->query($sql);
  
+
+
 if ($result->num_rows > 0) {
     // 输出数据
     while($row = $result->fetch_assoc()) {
-    	 echo json_encode($row);
+        $user = new User();
+        $user->ID = $row["ID"];
+        $user->X = $row["X"];
+        $user->Y = $row["Y"];
+        $user->Z = $row["Z"];
+        $user->speed = $row["speed"];
+        $user->pitch = $row["pitch"];
+        $user->roll = $row["roll"];
+        $user->azimuth = $row["azimuth"];
+        $user->time = $row["time"];
+        $data[] = $user;
 
-        // echo "id: " . $row["ID"]. " X: " . $row["X"]. " Y " . $row["Y"]." Z: " . $row["Z"]. "<br>";
     }
+    echo json_encode($data);
 } else {
     echo "0 结果";
 }
+
 $conn->close();
 ?>
